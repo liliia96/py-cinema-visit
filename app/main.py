ac5733c3dcf8449bef13e7a9749014c26fbe36e5
liliiaa96 <lili.noya24@gmail.com>
2026-09-14 00:19:34 +0300

    implement cinema vision solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,31 @@
-# write your imports here
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
+from app.people.customer import Customer
+from app.people.cinema_staff import Cleaner
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+def cinema_visit(customers: list,
+                 hall_number: int,
+                 cleaner: str,
+                 movie: str
+                 ) -> None:
+    customer_instances = []
+    for customer_dict in customers:
+        created_customer = Customer(
+            name=customer_dict["name"],
+            food=customer_dict["food"]
+        )
+        CinemaBar.sell_product(
+            product=created_customer.food,
+            customer=created_customer
+        )
+        customer_instances.append(created_customer)
+
+    cleaner_instance = Cleaner(name=cleaner)
+    hall_instance = CinemaHall(number=hall_number)
+
+    hall_instance.movie_session(
+        movie_name=movie,
+        customers=customer_instances,
+        cleaning_staff=cleaner_instance
+    )
